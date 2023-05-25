da7ebe334117eb1511beb8a61215ae4a19bf42d9
Aalisha Dalal <aalishadalal@cs.ucla.edu>
2023-05-25 07:48:48 -0700

    modify the header text

--- ocr_transliterate.py
+++ ocr_transliterate.py
@@ -14,30 +14,30 @@ def main():
     read_me_0 = st.markdown(new_title, unsafe_allow_html=True)
     read_me = st.markdown("""
     This project was built using Streamlit and Ai4-bharat transliteration  
-    to create Gujarati transliterated version of Patranks.""")
+    to create transliterated version of Patranks from Gujarati to English.""")
     st.sidebar.title("Select Activity")
     xlit_engine = load_model()
-    choice  = st.sidebar.selectbox("MODE",("About","OCR version (Patrank)","Gujarati Transliterated version (Patrank)"))
+    choice  = st.sidebar.selectbox("MODE",("About","Text from Image (Gujarati)","Transliterated version (English)"))
     text = ""
-    if choice == "OCR version (Patrank)":
+    if choice == "OCR version (Gujarati)":
         read_me_0.empty()
         read_me.empty()
         # GlobalHydra.instance().clear()
-        st.header('OCR version')
+        st.header('Text from Image (Gujarati)')
         st.subheader('Patrank screenshot:')
-        st.write('Please upload a screenshot image of the Patrank (png/ jpg)')
+        st.write('Please upload a screenshot image of the Patrank (png/ jpg) to get Gujarati text from Image')
         img_file = st.file_uploader(label="patrank_ocr_version", label_visibility="hidden")
         if img_file is not None:
             # To read image file buffer as a PIL Image:
             image = Image.open(img_file)
             text = pytesseract.image_to_string(image, lang="guj")
             st.write(text)
-    elif choice == "Gujarati Transliterated version (Patrank)":
-        st.header("Gujarati transliterated version (Patrank)")
+    elif choice == "Transliterated version (English)":
+        st.header("Transliterated version (English)")
         read_me_0.empty()
         read_me.empty()
         st.subheader('Patrank screenshot:')
-        st.write('Please upload a screenshot image of the Patrank (png/ jpg)')
+        st.write('Please upload a screenshot image of the Patrank (png/ jpg) to get a translilterated version in English.')
         img_file = st.file_uploader(label="patrank_transliterated_version", label_visibility="hidden")
         if img_file is not None:
             # To read image file buffer as a PIL Image:
