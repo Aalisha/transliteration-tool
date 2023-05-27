d718a1af6cad46bbf7c1024d5d9d375e9540137d
Aalisha Dalal <aalishadalal@cs.ucla.edu>
2023-05-26 23:38:29 -0700

    include form and loading logo

--- ocr_transliterate.py
+++ ocr_transliterate.py
@@ -9,6 +9,15 @@ def load_model():
     xlit_engine = XlitEngine(beam_width=10, src_script_type = "indic")
     return xlit_engine
 
+def load_text_from_image(image):
+    text = pytesseract.image_to_string(image, lang="guj")
+    return text
+
+def load_transliteration_from_image(image, xlit_engine):
+    text = pytesseract.image_to_string(image, lang="guj")
+    docx_text = xlit_engine.translit_sentence(text, 'gu')
+    return docx_text
+
 def main():
     new_title = '<p style="font-size: 42px;">Vachanamrutji Transliteration App!</p>'
     read_me_0 = st.markdown(new_title, unsafe_allow_html=True)
@@ -26,29 +35,33 @@ def main():
         st.header('Text from Image (Gujarati)')
         st.subheader('Patrank screenshot:')
         st.write('Please upload a screenshot image of the Patrank (png/ jpg) to get Gujarati text from Image')
-        img_file = st.file_uploader(label="patrank_ocr_version", label_visibility="hidden")
-        if img_file is not None:
+        with st.form("ocr_patrank"):
+            img_file = st.file_uploader(label="patrank_ocr_version", label_visibility="hidden")
+            submitted = st.form_submit_button("Submit")
+        if submitted:
             # To read image file buffer as a PIL Image:
             image = Image.open(img_file)
-            text = pytesseract.image_to_string(image, lang="guj")
-            st.write(text)
+            with st.spinner("Loading..."):
+                text = load_text_from_image(image)
+        st.write(text)
     elif choice == "Transliterated version (English)":
         st.header("Transliterated version (English)")
         read_me_0.empty()
         read_me.empty()
         st.subheader('Patrank screenshot:')
         st.write('Please upload a screenshot image of the Patrank (png/ jpg) to get a translilterated version in English.')
-        img_file = st.file_uploader(label="patrank_transliterated_version", label_visibility="hidden")
-        if img_file is not None:
+        with st.form("transliterate_patrank"):
+            img_file = st.file_uploader(label="patrank_transliterated_version", label_visibility="hidden")
+            submitted = st.form_submit_button("Submit")
+        if submitted:
             # To read image file buffer as a PIL Image:
             image = Image.open(img_file)
-            text = pytesseract.image_to_string(image, lang="guj")
-            docx_text = xlit_engine.translit_sentence(text, 'gu')
-            st.write(docx_text)
-            
+            with st.spinner("Loading..."):
+                text = load_transliteration_from_image(image, xlit_engine)
+        st.write(text)      
     elif choice == "About":   
         print()
         
 
 if __name__ == '__main__':
-	main()	
+	main()
