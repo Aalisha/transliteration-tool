d15ab2147660337c7c6fca8e7c66a12d144651c0
Aalisha Dalal <aalishadalal@cs.ucla.edu>
2023-09-02 17:42:48 -0700

    Update vachanamrutji_script.py

--- vachanamrutji_script.py
+++ vachanamrutji_script.py
@@ -2,14 +2,16 @@ import streamlit as st
 from PIL import Image
 import pytesseract
 from ai4bharat.transliteration import XlitEngine
+from ai4bharat.transliteration import XlitEngineTransformer_Indic2En
 
 img = Image.open('Raj_Gurudev.png')
 st.set_page_config(page_title='Transliteration tool', page_icon=img, layout="wide", initial_sidebar_state="auto")
 
-#@st.cache_resource
-#def load_model():
+@st.cache_resource
+def load_model():
 #    xlit_engine = XlitEngine(beam_width = 10, src_script_type = "indic")
-#    return xlit_engine
+    xlit_engine = XlitEngineTransformer_Indic2En(beam_width=4,rescore=True)
+    return xlit_engine
 	
 def main():
     new_title = '<p style="font-size: 42px;">Transliteration App!</p>'
